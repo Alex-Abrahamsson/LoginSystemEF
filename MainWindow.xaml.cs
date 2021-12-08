869efb571eca26b139ae8c0a98cb4c41f6d9da54
Alexander Abrahamsson <79103208+Alex-Abrahamsson@users.noreply.github.com>
2021-12-08 09:33:59 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginSystemEF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class Users
+        {
+            public int ID { get; set; }
+            [Required]
+            public string UserName { get; set; }
+            [Required]
+            public string Password { get; set; }
+            [Column(TypeName = "datetime")]
+            public DateTime CreationTime { get; set; }
+
+        }
+        public class AppDbContext : DbContext
+        {
+            public DbSet<Users> User { get; set; }
+            protected override void OnConfiguring(DbContextOptionsBuilder options)
+            {
+                options.UseSqlServer(@"Server=(local)\SQLEXPRESS;Database=LoginApp;Integrated Security=True");
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            string nameInput = nameInputBox.Text;
+            string password = passInputBox.Password;
+        }
+
+        private void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            using (var database = new AppDbContext())
+            {
+                var user = database.User.Where(u => u.UserName == nameInputBox.Text && u.Password == passInputBox.Password);
+
+                if (user.Count() > 0)
+                {
+                    MessageBox.Show("Logged in!");
+                }
+                else
+                {
+                    MessageBox.Show("Wrong Username or Password");
+                }
+            }
+        }
+
+        private void createBtn_Click(object sender, RoutedEventArgs e)
+        {
+            using (var database = new AppDbContext())
+            {
+                var checkIfExist = database.User.Where(u => u.UserName.Contains(nameInputBox.Text));
+                if (nameInputBox.Text.Count() != 0 && passInputBox.Password.Count() != 0)
+                {
+                    if (checkIfExist.Any() == false)
+                    {
+                        Users newUser = new Users()
+                        {
+                            UserName = nameInputBox.Text,
+                            Password = passInputBox.Password,
+                            CreationTime = DateTime.Now
+                        };
+                        database.User.Add(newUser);
+                        database.SaveChanges();
+                        MessageBox.Show("Created new account");
+                    }
+                    else
+                    {
+                        MessageBox.Show("A user with that name already exists!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Requires both username and password");
+                }
+            }
+        }
+    }
+}
---
+++ Migrations/20211207144917_First.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LoginSystemEF.Migrations
+{
+    public partial class First : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
